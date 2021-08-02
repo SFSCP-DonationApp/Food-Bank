deaeb4fb3a4de1a7dc54b0243cd424e1b2844c83
sicaramacuya <eric.morales@protonmail.com>
2021-08-02 17:48:12 -0400

    gets rid of storyboard and some file management
    This commits gets rid of the storyboard and setup the files a little. Adds a designated place for the signup views and login views.

---
+++ Donation App/Donation App/Signup/Controller/SignupVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Donation App
+//
+//  Created by Eric Morales on 8/2/21.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
+
