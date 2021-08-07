58fc089e0fe1cc78a0560b17ccc4f41bec8aa637
sicaramacuya <eric.morales@protonmail.com>
2021-08-07 14:16:20 -0400

    adds groups and files for the Donation Detail Page
    In this commit every file is empty.

---
+++ Donation App/Donation App/Donation Detail/Controller/DonationDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  DonationDetailVC.swift
+//  Donation App
+//
+//  Created by Eric Morales on 8/7/21.
+//
+
+import UIKit
+
+class DonationDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ Donation App/Donation App/Donation Detail/View/DonationDetailPage.swift
@@ -0,0 +1,12 @@
+//
+//  DonationDetailPage.swift
+//  Donation App
+//
+//  Created by Eric Morales on 8/7/21.
+//
+
+import UIKit
+
+class DonationDetailPage: UIView {
+
+}
