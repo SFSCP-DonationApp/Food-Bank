4110ba1787e2023a8c4340e0e7d1ee130d4c7d1b
sicaramacuya <eric.morales@protonmail.com>
2021-08-09 18:42:42 -0400

    adds initial groups and files the confirmation page

---
+++ Donation App/Donation App/Confirmation Page/Controller/ConfirmationPageVC.swift
@@ -0,0 +1,23 @@
+//
+//  ConfirmationPageVC.swift
+//  Donation App
+//
+//  Created by Eric Morales on 8/9/21.
+//
+
+import UIKit
+
+class ConfirmationPageVC: UIViewController {
+    // MARK: Properties
+    
+    // MARK: VC Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemPink
+    }
+    
+    // MARK: Methods
+
+}
---
+++ Donation App/Donation App/Confirmation Page/View/ConfirmationPage.swift
@@ -0,0 +1,15 @@
+//
+//  ConfirmationPage.swift
+//  Donation App
+//
+//  Created by Eric Morales on 8/9/21.
+//
+
+import UIKit
+
+class ConfirmationPage: UIView {
+    // MARK: Properties
+    
+    // MARK: Methods
+
+}
